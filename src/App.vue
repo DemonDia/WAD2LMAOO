e8828f290d32b517ab324920d6d71c4530f63de9
sherrillxtan <sherrilltan.2020@scis.smu.edu.sg>
2021-10-16 03:14:06 +0800

    alternative design of review
    testing my commit & push

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <Home :usertype ="usertype"/>
+  <!-- <Home /> -->
   <!-- <Modal/> -->
   <!-- <Project/> -->
   <!-- <Calendar/> -->
   <!-- <Dashboard/> -->
 <!-- <EmployeeToDo/> -->
 <!-- <authenticate/> -->
-<!-- <ProjectReview/> -->
+<ProjectReview :usertype ="usertype"/>
 </template>
 
 <script>
 // import Authenticate from "./components/Authenticate.vue"
 // import Modal from "./components/modal.vue"
-import Home from "./components/Home.vue"
+// import Home from "./components/Home.vue"
 // import Project from "./components/Project.vue"
 // import Calendar from "./components/Calendar.vue"
 // import Dashboard from "./components/Employee_Dashboard.vue"
 // import EmployeeToDo from "./components/EmployeeToDoList.vue"
-// import ProjectReview from "./components/ProjectReview.vue"
+import ProjectReview from "./components/ProjectReview.vue"
 export default {
   name: 'App',
   components: {
     // Authenticate
-    Home
+    // Home
     // Modal
     // Project
     // Calendar
     // Dashboard
     // EmployeeToDo
-    // ProjectReview
+    ProjectReview
 },
   data(){
     return{
