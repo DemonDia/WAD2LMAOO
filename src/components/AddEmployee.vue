4db238f7aa4add5d2184f9682ef4128581f4f0ab
sherrillxtan <sherrilltan.2020@scis.smu.edu.sg>
2021-11-02 17:58:20 +0800

    Updated pages

--- src/components/AddEmployee.vue
+++ src/components/AddEmployee.vue
@@ -3,7 +3,8 @@
         <Navbar/>
         <div class = "add-employee-container">
             <h1 id = "add-emp-header"  class="mb-4">Add Employee</h1>
-            <div class = "card onboarding-form">
+            
+            <div class = "card onboarding-form mb-5">
                 <table class = "form-table">
                     <tr>
                         <td id = "impt-info">
@@ -11,59 +12,55 @@
                                 <tr>
                                     <th>Name:</th>
                                     <td><input type  ="text" placeholder = "Employee Name" class = "form-control"/></td>
-
                                 </tr>
                                 <tr>
                                     <th>DOB:</th>
-                                    <td><input type ="date" class = "form-control"></td>
-
+                                    <td ><input type ="date" class = "form-control"></td>
                                 </tr>
 
                                 <tr>
                                     <th>Contact no:</th>
-                                    <td><input type ="number" maxlength="8" placeholder = "+65 XXXX XXXX"
-                                    class = "form-control"></td>
-
+                                    <td><input type ="number" maxlength="8" placeholder = "+65 XXXX XXXX" class = "form-control"></td>
                                 </tr>
                                 <tr>
                                     <th>Home Address:</th>
                                     <td><input type  ="text" placeholder = "Address" class = "form-control"/></td>
-
-
                                 </tr>
 
                                 <tr>
-                                    <th>Username:</th>
-                                    <td><input type  ="text" placeholder = "Username" class = "form-control"/></td>
-
+                                    <th>Email:</th>
+                                    <td><input type  ="text" placeholder = "Email" class = "form-control"/></td>
                                 </tr>
 
                                 <tr>
                                     <th>Password:</th>
                                     <td><input type  ="password" placeholder = "Enter password" class = "form-control"/></td>
                                 </tr>
+
                                 <tr>
                                     <th>Comments:</th>
                                     <td><textarea class="textarea" role="textbox"></textarea></td>
                                 </tr>
+
+                                <tr>
+                                    <th>Photo:</th>
+                                    <td id = "add-photo">
+                                        <div class = "card add-photo">
+                                            <!-- Employee Image -->
+                                        </div>
+                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                    </td>
+                                </tr>
+
                             </table>
-
-
-                        </td>
-                        <td id = "add-photo">
-                            <div class = "card add-photo">
-                                <!-- Employee Image -->
-                                
-                            </div>
-                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
                         </td>
                     </tr>
 
                 </table>
-                <div class = "button-container">
+                <div class = "button-container justify-content-center">
                     <router-link to = "/employees" class = "btn fire-btn">Cancel</router-link>
-                <!-- <button class = "btn fire-btn">Cancel</button> -->
-                <button class = "btn view-btn" v-on:click = "submit">Add</button>
+                    <!-- <button class = "btn fire-btn">Cancel</button> -->
+                    <button class = "btn view-btn" v-on:click = "submit">Add</button>
                 </div>
             </div>
         </div>
@@ -94,6 +91,29 @@ export default {
 }
 </script>
 <style scoped>
+
+table {
+  /* border: 1px solid black; */
+  table-layout: fixed;
+  width: 200px;
+}
+
+td {
+  /* border: 1px solid black; */
+  width: 100px;
+  overflow: hidden;
+}
+th{
+    /* border: 1px solid black; */
+    width: 20px;
+    overflow: hidden;
+}
+.card{
+    max-width: 100%;
+    height: auto;
+}
+
+
 #add-emp-header{
     width: max-content;
     margin: auto;
@@ -114,14 +134,13 @@ export default {
 .onboarding-form{
     background:#F8F8F8;
     margin: auto;
-    width:90%;
-    padding:10px;
+    width: 50%;
+    /* padding:10px; */
     align-items: center;
 }
 
 /* form */
 .form-table{
-    /* background:yellow; */
     width:90%;
 }
 #impt-info{
@@ -143,11 +162,14 @@ textarea{
 }
 .input-table tr th{
     text-align: right;
+    padding-right: 8px;
+    /* width: 40px; */
 }
+
 /* photo */
 .add-photo{
     background:white;
-    height: 400px;
+    height: 200px;
 }
 #add-photo{
     height:100%;
@@ -162,7 +184,9 @@ textarea{
     margin:10px;
 
 }
-
+/* #exampleFormControlFile1{
+    text-align: right;
+} */
 
 /* .view-btn{
     background: #6360FF;
@@ -185,20 +209,20 @@ margin: 5px;
     /* background: #6360FF; */
     background: rgb(253, 198, 97);
     border-radius: 5px;
-    width:200px;
+    width:100px;
     padding:10px;
     color: black;
-    margin: 5px;
+    margin: 15px;
     border: 1px solid black;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 2px 2px 0 rgba(0,0,0,0.19);
 }
 .fire-btn{
     border-radius: 5px;
-    width:200px;
+    width:100px;
     padding:10px;
     color: black;
     background:  rgb(255, 153, 153);
-    margin: 5px;
+    margin: 15px;
     border: 1px solid black;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 2px 2px 0 rgba(0,0,0,0.19);
 
